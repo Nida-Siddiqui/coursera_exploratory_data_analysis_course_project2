184b0593de746408dd1c42ea7b033380fd619ea6
Nida Siddiqui <nida.iliyas@gmail.com>
2017-04-01 17:59:43 -0700

    Create Plot2.R

---
+++ Plot2.R
@@ -0,0 +1,23 @@
+if(!file.exists(archiveFile)) {
+  archiveURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+  if(Sys.info()["sysname"] == "Darwin") {
+    	download.file(url=archiveURL,destfile=archiveFile,method="curl")
+    } else {
+    	download.file(url=url,destfile=archiveFile)
+    	}
+  }
+if(!(file.exists("summarySCC_PM25.rds") && 
+      	file.exists("Source_Classification_Code.rds"))) { unzip(archiveFile) }
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+###Question 2
+## Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510") from 1999 to 2008? 
+## Use the base plotting system to make a plot answering this question.
+subsetNEI  <- NEI[NEI$fips=="24510", ]
+
+aggregatedTotalByYear <- aggregate(Emissions ~ year, subsetNEI, sum)
+
+png('plot2.png')
+barplot(height=aggregatedTotalByYear$Emissions, names.arg=aggregatedTotalByYear$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' in the Baltimore City, MD emissions at various years'))
+dev.off()
